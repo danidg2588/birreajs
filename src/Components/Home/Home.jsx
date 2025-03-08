ba04d61be247ca8a6d190e1b18317989ae4ad534
danidg2588 <124293169+danidg2588@users.noreply.github.com>
2025-03-07 21:53:44 -0500

    flex

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,40 +7,35 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     const [menu,setMenu] = useState(false)
   return (
-    <>
-        <div className="mobile">
-            {menu?
-            <div className="main-menu">
-                <div className="close">
-                    <IoClose className='menu-close-icon' onClick={()=>setMenu(false)}/>
-                </div>
-                <div className="options">
-                    <Link className='menu-link' to="/">Inicio</Link>
-                    <Link className='menu-link' to="/">Autenticación</Link>
-                    <Link className='menu-link' to="/">Registrarme</Link>
-                </div>
+    <div className="mobile">
+        {menu?
+        <div className="main-menu">
+            <div className="close">
+                <IoClose className='menu-close-icon' onClick={()=>setMenu(false)}/>
             </div>
-            :null}
-            <div className="header">
-                <button className='menu-btn' onClick={()=>setMenu(true)}>
-                    <IoMenu className='menu-icon' />
-                </button>
-            </div>
-            <div className="main">
-                <h1>
-                    Entra, reserva y birrea
-                </h1>
-            </div>
-            <div className="footer">
-                <button className='btn'>
-                    Reservar
-                </button>
+            <div className="options">
+                <Link className='menu-link' to="/">Inicio</Link>
+                <Link className='menu-link' to="/">Autenticación</Link>
+                <Link className='menu-link' to="/">Registrarme</Link>
             </div>
         </div>
-        <div className="desktop">
-            <h1>Desde tu celular es más cool!</h1>
+        :null}
+        <div className="header">
+            <button className='menu-btn' onClick={()=>setMenu(true)}>
+                <IoMenu className='menu-icon' />
+            </button>
         </div>
-    </>
+        <div className="main">
+            <h1>
+                Entra, reserva y birrea
+            </h1>
+        </div>
+        <div className="footer">
+            <button className='btn'>
+                Reservar
+            </button>
+        </div>
+    </div>
   )
 }
 
